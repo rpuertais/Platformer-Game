72cfc31743ec612b044f8bd19037b2e9651095af
dnavarrom <dnavarrom@edu.tecnocampus.cat>
2026-01-21 10:42:58 +0100

    Jump Char
    Implementación de un salto básico del player, con las plataformas tageadas con Floor y con un Ground detector

---
+++ Assets/Scripts/Char/GroundChecker.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GroundChecker : MonoBehaviour
+{
+    public UnityEvent groundHitEvent;
+    public UnityEvent groundNoHitEvent;
+
+    //String array to know what tags are grounderable
+    public string[] collidableTags;
+
+    //On trigger enter compare the tags and send the hit event
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"{transform.parent.name} collided with {collision.name} with tag {collision.tag}");
+        if (TagCompare(collision))
+        {
+            groundHitEvent?.Invoke();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (TagCompare(collision))
+        {
+            groundHitEvent?.Invoke();
+        }
+    }
+
+    //On trigger exit compare the tags and send the NO hit event
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log($"{transform.parent.name} exited collision with {collision.name} with tag {collision.tag}");
+        if (TagCompare(collision))
+        {
+            groundNoHitEvent?.Invoke();
+        }
+    }
+
+    //Only compare tags
+    private bool TagCompare(Collider2D collision)
+    {
+        foreach (string tag in collidableTags)
+        {
+            if (collision.CompareTag(tag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}

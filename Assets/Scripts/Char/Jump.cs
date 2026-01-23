0fd5121d3146c18e77430cc63b4abd56b1355f1f
egutierrezm <egutierrezm@edu.tecnocampus.cat>
2026-01-23 11:30:57 +0100

    double jump

--- Assets/Scripts/Char/Jump.cs
+++ Assets/Scripts/Char/Jump.cs
@@ -11,7 +11,8 @@ public class Jump : MonoBehaviour
     public float DistanceToMaxHeight;
     public float SpeedHorizontal;
     public float PressTimeToMaxJump;
-
+    public int jumpCount = 0;
+    public int maxJumps = 2;
     private float jumpStartedTime;
 
     private float lastVelocityY;
@@ -28,10 +29,21 @@ public class Jump : MonoBehaviour
 
     public void OnJumpStarted()
     {
+        if (jumpCount >= maxJumps)
+        {
+            return;
+        }
+        if (grounded) 
+        { 
+            jumpCount = 0; 
+        }
+        grounded = false;
+        jumpCount++;
         SetGravity();
         var velocity = new Vector2(rigidbody.linearVelocity.x, GetJumpForce());
         rigidbody.linearVelocity = velocity;
         jumpStartedTime = Time.time;
+
     }
 
     public void OnJumpFinished()
@@ -40,16 +52,17 @@ public class Jump : MonoBehaviour
         rigidbody.gravityScale *= fractionOfTimePressed;
     }
 
-    /*
+    
     public void GroundHitCallBack()
     {
         grounded = true;
+        jumpCount = 0;
     }
 
     public void GroundNoHitCallBack()
     {
         grounded = false;
-    }*/
+    }
 
     private void TweakGravity()
     {
